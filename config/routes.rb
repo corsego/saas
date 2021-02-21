b3ed1cfadd353ebc250f624e1fcaf8ab192b9c70
Yaro <yshmarov@gmail.com>
2021-02-21 22:53:05 +0000

    sign user in after confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "users/omniauth_callbacks",
+    confirmations: 'users/confirmations'
   }
 
   root "static_public#landing_page"
