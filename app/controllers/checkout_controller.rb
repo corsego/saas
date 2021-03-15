b8e0c61e160af5c2f1d0bbd0703cdf0da772f5fc
Yaro <yshmarov@gmail.com>
2021-03-15 20:32:12 +0000

    improve stripe checkout_controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,22 +3,15 @@ class CheckoutController < ApplicationController
   include RequireTenant # no current_tenant = no access to entire controller. redirect to root
 
   def create
-    tenant = Tenant.find(params[:id])
-
-    if tenant.nil?
-      redirect_to root_path
-      return
-    end
-
     @session = Stripe::Checkout::Session.create(
       customer: current_tenant.stripe_customer_id,
       payment_method_types: ['card'],
       line_items: [{
-          name: tenant.plan.name,
-          description: "max_members: #{tenant.plan.max_members}",
-          amount: tenant.plan.amount,
-          currency: tenant.plan.currency,
-          quantity: 1
+        name: current_tenant.plan.name,
+        description: "max_members: #{current_tenant.plan.max_members}",
+        amount: current_tenant.plan.amount,
+        currency: current_tenant.plan.currency,
+        quantity: 1
       }],
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
@@ -33,4 +26,9 @@ class CheckoutController < ApplicationController
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
   end
+
+  def cancel
+    redirect_to tenant_url(current_tenant), notice: "Transaction: Cancelled"
+  end
+
 end
