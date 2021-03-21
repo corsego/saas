20b0de08006672cc01941f7f30ee4e569c99dc00
Yaro <yshmarov@gmail.com>
2021-03-21 17:30:46 +0000

    errors_controller works without devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
-  config.hosts << "656f956582314da381b9e27897dc04bf.vfs.cloud9.eu-central-1.amazonaws.com"
+  config.hosts << "85e6ea410d9e4eb3b85d27b6177fa024.vfs.cloud9.eu-central-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = {host: "https://656f956582314da381b9e27897dc04bf.vfs.cloud9.eu-central-1.amazonaws.com"}
+  config.action_mailer.default_url_options = {host: "https://85e6ea410d9e4eb3b85d27b6177fa024.vfs.cloud9.eu-central-1.amazonaws.com"}
   config.action_mailer.delivery_method = :test
 
   # In the development environment your application's code is reloaded on
