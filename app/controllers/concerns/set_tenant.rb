5450d5fe1dc2f22090c0c8cc35e2f56e8a9043b4
Ahmed Nadar <AhmedNadar@users.noreply.github.com>
2021-05-05 23:32:14 +0400

    Refactor set_tenant method
    Eliminate use conditional IF as possible. Now it is more readable and clear.

--- app/controllers/concerns/set_tenant.rb
+++ app/controllers/concerns/set_tenant.rb
@@ -5,28 +5,10 @@ module SetTenant
     set_current_tenant_through_filter # acts_as_tenant
     before_action :set_tenant
 
-    # def set_tenant
-    #   return set_current_tenant(nil) if current_user
-    #   return set_current_tenant(nil) if current_user.tenant_id.present?
-    #   return set_current_tenant(nil) if current_user.tenants.include?(current_user.tenant)
-    #   set_current_tenant(current_user.tenant)
-    # end
-
     def set_tenant
-      if current_user
-        if current_user.tenant_id.present?
-          if current_user.tenants.include?(current_user.tenant)
-            # current_account = Tenant.find_by(id: current_user.tenant_id) #does not require belongs_to relationship
-            # requires belongs_to relationship
-            current_account = current_user.tenant
-            # User.find_by(email: "wawa@w.com").update(tenant_id: 2)
-            set_current_tenant(current_account)
-          else
-            set_current_tenant(nil)
-          end
-        else
-          set_current_tenant(nil)
-        end
+      if current_user && current_user.tenant_id.present? && current_user.tenants.include?(current_user.tenant)
+        current_account = current_user.tenant
+        set_current_tenant(current_account)
       else
         set_current_tenant(nil)
       end
